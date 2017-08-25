50ef41993add70906a146c0e6c46194dc896caed
umurcg <umurcg@gmail.com>
2017-08-25 18:40:46 +0300

    fixed lag issue

---
+++ arAudioVisualizer/Assets/AudioVisulization/AudioPeer.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Linq;
+/// <summary>
+/// Gets spectrum of frequencies at every fixed update and share this through network
+/// </summary>
+
+public class AudioPeer : NetworkBehaviour {
+
+    
+    AudioSource audioSource;
+    
+    //Samples should be get at every update. After assigning samples every frequency visulize must be updated in network
+    //public SyncListFloat samples =new SyncListFloat();
+        
+    //Frequency visulizer of scene
+    public FrequencyVisulizer visulizer;
+
+    public int spectrumLength = 512;
+
+    NetworkClient myClient;
+
+
+    // Use this for initialization
+    void Start () {
+
+
+               
+        audioSource = GetComponent<AudioSource>();
+        //If not server than there shouldn't be audio source
+        if (!isServer)
+        {
+            Debug.Log("Destroying source");
+            Destroy(audioSource);
+        }
+  
+
+ 
+	}
+
+    public void OnConnected(NetworkConnection conn, NetworkReader reader)
+    {
+       conn.SetChannelOption(Channels.DefaultReliable, ChannelOption.MaxPendingBuffers, 64);
+        Debug.Log("New client is connected");
+    }
+
+
+    [ContextMenu("Change pending buffer rate")]
+    public void setChannelBufferLimit()
+    {
+        connectionToClient.SetChannelOption(Channels.DefaultReliable, ChannelOption.MaxPendingBuffers, 64);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //If server than get spectrum from audio
+        if (isServer) GetSpectrumAudioSource();
+	}
+
+    void GetSpectrumAudioSource()
+    {
+        //If no audio clip then return
+        if (audioSource.clip == null) return;
+
+        //create float array for spectrum
+        float[] samplesArray=new float[spectrumLength];
+        
+        //get spectrum data
+        audioSource.GetSpectrumData(samplesArray, 0, FFTWindow.Blackman);
+
+        ////Fill sample sync array 
+        //for(int i = 0; i < spectrumLength; i++)
+        //{
+        //    samples[i] = samplesArray[i];
+        //}
+
+        //Update all visulizer in network
+        RpcUpdateVisulizer(samplesArray);
+    }
+
+    [ClientRpc]
+    void RpcUpdateVisulizer(float[] samples)
+    {
+        visulizer.updateVisulizer(samples);
+    }
+
+
+
+}
---
+++ arAudioVisualizer/Assets/BasicCharAnimations.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MovementEffects;
+
+//_BasicCharAnimation.cs
+//_Dependent to: 
+
+//This script controls animations of player character.
+//It adjust walking animation according to speed of object.
+//It triggers idle animation when object doesnt move.
+
+
+public class BasicCharAnimations : MonoBehaviour{
+    public float threshold=0.01f;
+
+
+    public string animName="Walk";
+    Vector3 lastPosition;
+    Animator anim;
+    AnimationClip walk;
+    public float speedFactor = 0.5f;
+
+
+    public int delayForStop = 50;
+    int timer=0;
+    
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+
+        
+
+//#if UNITY_EDITOR
+//        //Debug.Log("It is in editor");
+      
+//        return;
+//#else
+    
+//          addOptimization();
+//#endif
+
+        //lastRotate = transform.rotation;
+
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        
+        if (lastPosition == Vector3.zero) lastPosition = transform.position;
+
+        float dist = Vector3.Distance(transform.position, lastPosition);
+        float speed = speedFactor * dist / Time.deltaTime;
+
+        //float rotation = Quaternion.Angle(transform.rotation, lastRotate);
+
+        //if (gameObject == CharGameController.getActiveCharacter())
+        //{
+        //    Debug.Log("Gameobject: " + gameObject.name + " Dist is " + dist + " threshold is " + threshold);
+        //    if (dist <= threshold && timer>delayForStop) { Debug.Log("STOPPINGGG"); }
+        //}
+
+        if (dist > threshold)
+        {
+            anim.speed = speed;
+            //print(dist);
+            anim.SetBool(animName, true);
+
+
+        }        
+        else if(anim.GetBool(animName)==true)
+        {
+            if (timer < delayForStop)
+            {
+                timer++;
+            }
+            else
+            {
+                timer = 0;
+                anim.speed = 1;
+                anim.SetBool(animName, false);
+
+            }
+        }
+
+
+        lastPosition = transform.position;
+        
+    }
+
+
+
+    ////This funciton adds optimization script to renderer object.
+    ////It makes disable and enable animation and update functiion of this sciprt when renderer invisible and visible respectevly.
+    //void addOptimization()
+    //{
+    //    if (GetComponentInChildren<DisableEnableBACInParent>()) return; 
+
+    //    Renderer rend=GetComponent<Renderer>();
+    //    if (!rend) rend= GetComponentInChildren<Renderer>();
+
+    //    rend.gameObject.AddComponent<DisableEnableBACInParent>();
+    //}
+
+}
